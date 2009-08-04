2db0579c2dc9ee1a85c0ed90927072d80f929030
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-04 15:07:47 -0500

    git-svn-id: http://svn.symfony-project.com/plugins/pkMediaCMSSlotsPlugin/trunk@20777 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/filter/doctrine/PluginpkContextCMSPDFSlotFormFilter.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginpkContextCMSPDFSlot form.
+ *
+ * @package    filters
+ * @subpackage pkContextCMSPDFSlot *
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginpkContextCMSPDFSlotFormFilter extends BasepkContextCMSPDFSlotFormFilter
+{
+}
---
+++ lib/form/doctrine/PluginpkContextCMSPDFSlotForm.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginpkContextCMSPDFSlot form.
+ *
+ * @package    form
+ * @subpackage pkContextCMSPDFSlot
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginpkContextCMSPDFSlotForm extends BasepkContextCMSPDFSlotForm
+{
+}
