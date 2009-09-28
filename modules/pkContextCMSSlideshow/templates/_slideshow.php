e3cef59bdea20772f51e120b44b4ef626990f5b8
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-28 16:25:10 -0500

    added showImage(n) event to slideshow slot)
    git-svn-id: http://svn.symfony-project.com/plugins/pkMediaCMSSlotsPlugin/trunk@22581 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMSSlideshow/templates/_slideshow.php
+++ modules/pkContextCMSSlideshow/templates/_slideshow.php
@@ -36,84 +36,101 @@
 </ul>
 
 <?php if (count($items) > 1): ?>
-<script type='text/javascript'>
-$(function() {
+	<script type='text/javascript'>
+		$(document).ready(function() {
 
-	var position = 0;
-	var img_count = <?php echo count($items) ?>-1;
+			var position = 0;
+			var img_count = <?php echo count($items) ?>-1;
+			var slideshowItems = $('#pk-slideshow-<?php echo $id ?> .pk-slideshow-item');
+			$('.pk-context-media-show-item').hide();
+			$('#pk-slideshow-item-<?php echo $id ?>-'+position).show();
 
-	$('#pk-slideshow-<?php echo $id ?> .pk-slideshow-item').hide(); // Hide all slideshow items to start
-	$('#pk-slideshow-item-<?php echo $id ?>-'+position).show(); // Show the first slideshow item
+			var intervalEnabled = <?php echo ($options['interval'])? 1:0; ?>;
 
-	var intervalEnabled = <?php echo ($options['interval'])? 1:0; ?>;
+		slideshowItems.attr('title', 'Click For Next Image');
 	
-	$('#pk-slideshow-<?php echo $id ?> .pk-slideshow-item').attr('title', 'Click For Next Image');
-  
-  $('#pk-slideshow-<?php echo $id ?> .pk-slideshow-item').click(function() {
-	  intervalEnabled = false;
-	  next();
-  });
-
-	$('#pk-slideshow-controls-<?php echo $id ?> .pk-slideshow-controls-previous').click(function(event){
-		event.preventDefault();
-		intervalEnabled = false;
-		previous();
-	});
-
-	$('#pk-slideshow-controls-<?php echo $id ?> .pk-slideshow-controls-next').click(function(event){
-		event.preventDefault();
-		intervalEnabled = false;
-		next();
-	});
+		$('#pk-slideshow-<?php echo $id ?>').bind('showImage', function(e, num){
+			position = num;
+			slideshowItems.hide();
+			$('#pk-slideshow-item-<?php echo $id ?>-'+position).fadeIn('slow');				
+		})
 	
-	// Catch the anchor click and unset the parent click!
-	$('.pk-slideshow-description a').mouseup(function(){ 
-		$(this).parents('.pk-slideshow-item').unbind('click');
-	});
+	  slideshowItems.click(function(event) {
+			event.preventDefault();
+			$(this).attr('title','Click For Next Image');
+			if (position <= img_count)
+			{
+				position++;
+				if (position == img_count+1 ) { position = 0; }
+				slideshowItems.hide();
+				$('#pk-slideshow-item-<?php echo $id ?>-'+position).fadeIn('slow');	
+			}
+	  });
 
-  function previous() 
-  {
-  	if (position >= 0)
-		{
-			position--;
-			if (position < 0 ) { position = img_count; }
-			$('#pk-slideshow-<?php echo $id ?> .pk-slideshow-item').hide();
-			$('#pk-slideshow-item-<?php echo $id ?>-'+position).fadeIn('slow');		
-		}
-		interval();
-  }
+		$('#pk-slideshow-controls-<?php echo $id ?> .pk-slideshow-controls-previous').click(function(event){
+			event.preventDefault();
+			intervalEnabled = false;
+			previous();
+		});
+
+		$('#pk-slideshow-controls-<?php echo $id ?> .pk-slideshow-controls-next').click(function(event){
+			event.preventDefault();
+			intervalEnabled = false;
+			next();
+		});
+
+		$('.pk-slideshow-controls li').hover(function(){
+			$(this).css('background-position','0 -20px');		
+		},function(){
+			$(this).css('background-position','0 0');					
+		})
+
+	  function previous() 
+	  {
+	  	if (position >= 0)
+			{
+				position--;
+				if (position < 0 ) { position = img_count; }
+				slideshowItems.hide();
+				$('#pk-slideshow-item-<?php echo $id ?>-'+position).fadeIn('slow');		
+			}
+			interval();
+	  }
  
-  function next()
-  {
-  	if (position <= img_count)
-  	{
-  		position++;
-  		if (position == img_count+1 ) { position = 0; }
-  		$('#pk-slideshow-<?php echo $id ?> .pk-slideshow-item').hide();
-  		$('#pk-slideshow-item-<?php echo $id ?>-'+position).fadeIn('slow');			
-  	}
-  	interval();
-  }
-  var intervalTimeout = null;
-  function interval()
-  {
-    if (intervalTimeout)
-    {
-      clearTimeout(intervalTimeout);
-    }
-    if (intervalEnabled)
-    {
-  	  intervalTimeout = setTimeout(next, <?php echo $options['interval'] ?> * 1000);
-  	}
-  }
-  interval();
-});
-</script>
-<?php elseif (count($items) == 1): ?>
-<script type="text/javascript" charset="utf-8">
-  <?php // Make sure a single-image slideshow is not hidden entirely ?>
-	$(document).ready(function() {
-    $('#pk-slideshow-item-<?php echo $id ?>-0').show();  
+	  function next()
+	  {
+	  	if (position <= img_count)
+	  	{
+	  		position++;
+	  		if (position == img_count+1 ) { position = 0; }
+	  		slideshowItems.hide();
+	  		$('#pk-slideshow-item-<?php echo $id ?>-'+position).fadeIn('slow');			
+	  	}
+	  	interval();
+	  }
+	  
+		var intervalTimeout = null;
+	  function interval()
+	  {
+	    if (intervalTimeout)
+	    {
+	      clearTimeout(intervalTimeout);
+	    }
+	    if (intervalEnabled)
+	    {
+	  	  intervalTimeout = setTimeout(next, <?php echo $options['interval'] ?> * 1000);
+	  	}
+	  }
+	  interval();
+	
 	});
-</script>
+	</script>
+
+<?php elseif (count($items) == 1): ?>
+
+	<script type="text/javascript" charset="utf-8">
+	  <?php // Make sure a single-image slideshow is not hidden entirely ?>
+	        $('#pk-slideshow-item-<?php echo $id ?>-0').show();  
+	</script>
+
 <?php endif ?>
