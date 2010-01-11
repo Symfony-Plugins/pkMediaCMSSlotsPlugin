0dd54dd923f9fe7d01ab320afaae16b4b8967e19
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-11 15:17:40 -0600

    Enhanced slideshow to display the first image if nojavascript
    git-svn-id: http://svn.symfony-project.com/plugins/pkMediaCMSSlotsPlugin/trunk@26490 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMSSlideshow/templates/_slideshow.php
+++ modules/pkContextCMSSlideshow/templates/_slideshow.php
@@ -22,7 +22,7 @@
       $dimensions['format']),
     $item->embed) ?>
 
-  <li class="pk-slideshow-item" id="pk-slideshow-item-<?php echo $id ?>-<?php echo $n ?>">
+  <li class="pk-slideshow-item" id="pk-slideshow-item-<?php echo $id ?>-<?php echo $n ?>" <?php echo ($first)? 'style="display:list-item;"':''; ?>>
 		<?php include_partial('pkContextCMSSlideshow/slideshowItem', array('item' => $item, 'id' => $id, 'embed' => $embed, 'n' => $n,  'options' => $options)) ?>
 	</li>
 <?php $first = false; $n++; endforeach ?>
